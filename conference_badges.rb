cf7c55bdfa1c403812078c179ed6ca27ee915c92
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-01 21:38:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,7 @@
 # Write your code here.
 
 def badge_maker (name)
-    name = "Arel"
-    puts "Hello, my name is #{name}."
+    return "Hello, my name is #{name}."
   end
   
   def batch_badge_creator (attendees)
