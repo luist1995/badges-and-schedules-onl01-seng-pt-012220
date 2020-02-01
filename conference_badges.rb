02656fbd61ba389d0f0ca06166fe80ed199a7b0f
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-01 21:35:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,11 @@ def badge_maker (name)
   end
   
 def assign_rooms (attendees)
-  
+  new_array = []
+  attendees.each_with_index do |name, index|
+    new_array.push ("Hello, #{name}! You'll be assigned to room #{index + 1}!.")
+  end 
+  new_array
+end
     
     
